5cba9a4b2f60eea933b0aa8a337ae141490c394c
daljeet mathur <daljeet.mathur2002@gmail.com>
2026-01-17 14:03:41 +0530

    add lodder

--- My portfoliyo/js/custom.js
+++ My portfoliyo/js/custom.js
@@ -8,3 +8,15 @@ const navmanu = document.querySelector(".nav");
 	navmanu.classList.toggle("active");
 	
 	})
+
+
+	// lodder
+
+	// lodder
+  window.addEventListener("load", () => {
+      setTimeout(() => {
+        document.getElementById("loader-wrapper").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        document.body.style.overflow = "auto";
+      }, 3000); // still 1 second loader
+    });
